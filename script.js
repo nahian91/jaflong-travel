d27b33c8b8506655b4169f85668abe62d8757926
nahian28 <nahiansylhet@gmail.com>
2023-03-30 02:02:34 +0600

    Init

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    let menu_icon = document.querySelector('.menu-icon i');
+    let menu_list = document.querySelector('.header ul');
+
+    menu_icon.onclick = () => {
+        menu_icon.classList.toggle('bx-x');
+        menu_list.classList.toggle('open')
+    }
+    
+
+})
